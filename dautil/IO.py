a0d4c9e0732b2445b2d9890096984133a2037423
Kolen Cheung <christian.kolen@gmail.com>
2017-10-10 21:04:51 -0700

    IO.py: add makedirs

--- dautil/IO.py
+++ dautil/IO.py
@@ -1,3 +1,4 @@
+import os
 import pickle
 
 
@@ -6,3 +7,17 @@ def read_pkl2(path):
     '''
     with open(path, 'rb') as f:
         return pickle.load(f) if PY2 else pickle.load(f, encoding='latin1')
+
+
+def makedirs(path):
+    '''makesdirs if not exist while avoiding race condition
+    catch the case that path is file, whether initially or in a race condition
+    '''
+    if os.path.isfile(path):
+        raise OSError
+    if not os.path.isdir(path):
+        try:
+            os.makedirs(path)
+        except OSError:
+            if not os.path.isdir(path):
+                raise
