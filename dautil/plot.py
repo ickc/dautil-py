5d65b63bdf00b35775c25c9b5dda5a5df0727b9f
Kolen Cheung <christian.kolen@gmail.com>
2017-10-13 16:27:45 -0700

    plot.py: add save decorator

--- dautil/plot.py
+++ dautil/plot.py
@@ -1,6 +1,46 @@
+import matplotlib
+matplotlib.use('pgf')
+
+from dautil.IO import makedirs
 import numpy as np
+import os
 import matplotlib.pyplot as plt
 import seaborn as sns
+from functools import wraps
+# from matplotlib2tikz import save as tikz_save
+
+
+def save(f):
+    '''a decorator to add keyword filename to the function args
+    the function f is expected to be a procedure that uses matplotlib
+    the figure produced is saved to the path specified in filename
+    dispatch according to the filename extension
+    '''
+    @wraps(f)
+    def f_decorated(*args, filename=None, **kwargs):
+        makedirs(os.path.dirname(filename))
+
+        ext = os.path.splitext(filename)[1][1:] if filename else None
+
+        if ext in ('pdf', 'pgf'):
+            # setup fonts using Latin Modern
+            matplotlib.rcParams.update({
+                "font.family": ["serif"],
+                "font.serif": ["Latin Modern Roman"],
+                "font.sans-serif": ["Latin Modern Sans"],
+                "font.monospace": ["Latin Modern Mono"]
+            })
+
+        f(*args, **kwargs)
+
+        if filename:
+            if ext == "tikz":
+                tikz_save(filename,
+                          figureheight='\\figureheight',
+                          figurewidth='\\figurewidth')
+            else:
+                plt.savefig(filename)
+    return f_decorated
 
 
 def plot_x_y_complex(x, y):
