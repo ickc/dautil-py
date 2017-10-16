ee0df8452c0bdb82afecf65bcbb270ef30c279cb
Kolen Cheung <christian.kolen@gmail.com>
2017-10-15 17:56:52 -0700

    plot.py: refactor from abx (private)

--- dautil/plot.py
+++ dautil/plot.py
@@ -4,10 +4,13 @@ matplotlib.use('pgf')
 from dautil.IO import makedirs
 import numpy as np
 import os
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from functools import wraps
 # from matplotlib2tikz import save as tikz_save
+
+idx = pd.IndexSlice
 
 
 def save(f):
@@ -44,6 +47,58 @@ def save(f):
                 plt.savefig(filename)
     return f_decorated
 
+# plot pandas DataFrame ################################################
+
+def plot_unique(df, col_select, col_plot):
+    '''plot the values of col_plot
+    per unique value in col_select
+    '''
+    for value in df[col_select].unique():
+        sns.kdeplot(df[df[col_select] == value][col_plot], label=value)
+    plt.title(col_plot)
+
+
+def plot_unique_index(df, idx_select, col_plot):
+    '''plot the values of col_plot
+    per index from index level idx_select
+    TODO: currently, assuming df has 2 levels of MultiIndex. Generalize this.
+    '''
+    for value in df.index.levels[idx_select]:
+        if idx_select == 0:
+            indexslice = idx[value, :]
+        elif idx_select == 1:
+            indexslice = idx[:, value]
+        sns.kdeplot(df.loc[indexslice, :][col_plot], label=value)
+    plt.title(col_plot)
+
+
+def plot_unique_index_binned(df, idx_select, col_plot, bin_width=None):
+    '''similar to plot_unique_index, but the values of index in level idx_select
+    is binned instead
+    TODO: currently, assuming df has 2 levels of MultiIndex. Generalize this.
+
+    Examples
+    --------
+    >>> plot_unique_index_binned(df_masked, 0, 'ampparams2_1', bin_width=pd.Timedelta('10 days'))
+    '''
+    values = df.index.levels[idx_select]
+    n = values.shape[0]
+    idx_current = values[0]
+    idx_end = values[-1]
+    while idx_current <= idx_end:
+        idx_new = idx_current + bin_width
+        if idx_select == 0:
+            indexslice = idx[idx_current:idx_new, :]
+        elif idx_select == 1:
+            indexslice = idx[:, idx_current:idx_new]
+        try:
+            sns.kdeplot(df.loc[indexslice, :][col_plot], label=idx_current)
+        except (ValueError, ZeroDivisionError):
+            print('No data between {} and {}.'.format(idx_current, idx_new))
+        idx_current = idx_new
+    plt.title(col_plot)
+
+########################################################################
 
 def plot_x_y_complex(x, y):
     '''x is a real array.
