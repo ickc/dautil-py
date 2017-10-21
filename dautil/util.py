9533e50468773d777e53959ff1e2c1b5b9c0dfa3
Kolen Cheung <christian.kolen@gmail.com>
2017-10-20 18:47:35 -0700

    util.py: add df_linregress

--- dautil/util.py
+++ dautil/util.py
@@ -2,6 +2,7 @@ import numpy as np
 import operator
 import pandas as pd
 from functools import reduce
+import scipy
 
 # summarize ############################################################
 
@@ -139,3 +140,28 @@ def insert_index_level(df, level, name, value):
     df.set_index(name, append=True, inplace=True)
 
     return df.reorder_levels(order)
+
+########################################################################
+
+def df_linregress(df, grouplevel=0, regresslevel=1, regressindex=2, regressorder=2):
+    '''per level ``grouplevel``, perform a linregress of each column vs. level ``regresslevel``
+    assumes df has MultiIndex of 2 levels, behavior undefined otherwise.
+
+    ``regressindex`` and ``regressorder`` control which amount the 5 outputs from scipy.stats.linregress is returned
+    If ``regressindex`` is None, return all as an object, else, only return the stat at this index.
+    In the latter case, the stat will be raised to the power ``regressorder``, which is useful to return $R^2$.
+    TODO: if regressindex is None, return a DataFrame that the 5 elements returned at MultiIndex level-2.
+    '''
+    df_grouped = df.groupby(level=grouplevel)
+    
+    if regressindex is None:
+        _linregress = lambda x: scipy.stats.linregress(x.reset_index(level=regresslevel).as_matrix())
+    elif regressorder == 1:
+        _linregress = lambda x: scipy.stats.linregress(x.reset_index(level=regresslevel).as_matrix())[regressindex]
+    else:
+        _linregress = lambda x: scipy.stats.linregress(x.reset_index(level=regresslevel).as_matrix())[regressindex]**regressorder
+
+    dfs = (df_grouped.get_group(key).apply(_linregress).to_frame(key).transpose() for key in df_grouped.groups.keys())
+    df_final = pd.concat(dfs)
+    df_final.sort_index(inplace=True)
+    return df_final
