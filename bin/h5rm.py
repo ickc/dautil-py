795e49e13e8672b32794f24d3e88f5e92af2755f
Kolen <christian.kolen@gmail.com>
2018-02-08 02:26:38 -0800

    add h5rm.py: delete dataset in hdf5 files

---
+++ bin/h5rm.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import argparse
+import h5py
+from functools import partial
+
+from dautil.util import get_map_parallel
+
+__version__ = 0.1
+
+
+def del_dataset(dataset, h5path):
+    with h5py.File(h5path) as f:
+        for i in dataset:
+            try:
+                del f[i]
+            except KeyError:
+                pass
+
+
+def main(args):
+    map_parallel(partial(del_dataset, args.dataset), args.input)
+
+
+def cli():
+    parser = argparse.ArgumentParser(description='Delete datasets from input HDF5 files.')
+
+    parser.add_argument('-i', '--input', nargs='*', required=True,
+                        help='a list of path to HDF5 files where datasets are to be deleted from.')
+    parser.add_argument('-d', '--dataset', nargs='*', required=True,
+                        help='a list of the names of datasets to be deleted.')
+    parser.add_argument('-p', type=int, default=1,
+                        help="No. of parallel processes using multiprocessing.")
+    parser.add_argument('-v', '--version', action='version',
+                        version='%(prog)s {}'.format(__version__))
+
+    args = parser.parse_args()
+
+    global map_parallel
+    map_parallel = get_map_parallel(args.p)
+
+    main(args)
+
+
+if __name__ == "__main__":
+    cli()
