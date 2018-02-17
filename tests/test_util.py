2ccf3f4f362905fd542f152e309e689a4276e8f8
Kolen <christian.kolen@gmail.com>
2018-02-16 20:01:23 -0800

    test_util.py: test_arange_inv: explicit float

--- tests/test_util.py
+++ tests/test_util.py
@@ -4,7 +4,7 @@ from .context import util
 
 
 def test_arange_inv():
-    start, stop, step = 0., 4., 1 / 40000
+    start, stop, step = 0., 4., 1. / 40000.
     array = np.arange(start, stop, step)
     inv_array = util.arange_inv(array)
     np.testing.assert_array_equal(np.arange(start, stop, step), np.arange(*inv_array))
