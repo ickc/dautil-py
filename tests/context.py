0f26a1a788eb7a0f0333a3b5dff056b980618f56
Kolen <christian.kolen@gmail.com>
2018-03-13 16:42:53 -0700

    mv dautil/datetime.py dautil/date_time.py: avoid namespace collision with standard lib.

--- tests/context.py
+++ tests/context.py
@@ -2,4 +2,4 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-from dautil import util, datetime
+from dautil import util, date_time
--- tests/test_datetime.py
+++ tests/test_datetime.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-from .context import datetime
+from .context import date_time
 
 
 def test():
     sample = '20140725_005302'
-    np_time = datetime.strptime_np(sample)
-    assert datetime.strptime_np_general(sample) == np_time
+    np_time = date_time.strptime_np(sample)
+    assert date_time.strptime_np_general(sample) == np_time
 
-    assert datetime.strttime_np(np_time) == sample
-    assert datetime.strttime_np_general(np_time) == sample
+    assert date_time.strttime_np(np_time) == sample
+    assert date_time.strttime_np_general(np_time) == sample
