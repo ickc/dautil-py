6c836f7a7ac54bbf3c3130b3a2b56b22b594eec8
Kolen <christian.kolen@gmail.com>
2018-10-19 02:50:14 -0700

    extreme_video_duration.py: parallelize and better error handling

--- bin/extreme_video_duration.py
+++ bin/extreme_video_duration.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 
 import argparse
+from collections import deque
 import numpy as np
 from pathlib import Path
-import pandas as pd
+from functools import partial
 import scipy
 import sys
+from concurrent.futures import ProcessPoolExecutor
 from moviepy.editor import VideoFileClip
 
 from dautil.stat import get_cutoffs
 
-__version__ = '0.1.1'
+__version__ = '0.2'
 
 EXTS = {
     '.mp4',
@@ -23,36 +25,38 @@ EXTS = {
     '.mpg',
 }
 
+def get_duration(pathname):
+    try:
+        return VideoFileClip(str(pathname)).duration
+    except OSError:
+        return np.nan
 
-def main_per_path(path):
-    path = Path(path)
-    files = pd.DataFrame(
-        [
-            file
-            for file in path.glob('**/*')
-            if (file.is_file() or file.is_symlink()) and file.suffix in EXTS
-        ],
-        columns=('Path',)
-    )
-    files['duration'] = files['Path'].map(lambda file: VideoFileClip(str(file)).duration)
-    cutoffs = get_cutoffs(files.duration)
-    return files.loc[(files.duration < cutoffs[0]) | (files.duration > cutoffs[1]), 'Path']
+
+def main_per_path(directory, file=sys.stdout):
+    pathnames = np.array([
+        pathname
+        for pathname in Path(directory).glob('**/*')
+        if (pathname.is_file() or pathname.is_symlink()) and pathname.suffix in EXTS
+    ])
+    with ProcessPoolExecutor() as executor:
+        durations = np.array(list(executor.map(get_duration, pathnames)), dtype=np.float32)
+    idx_nan = np.isnan(durations)
+    idx_nan
+    data = durations[~idx_nan]
+    cutoffs = get_cutoffs(data) if data.size else np.array((np.NINF, np.inf))
+    for pathname in (pathnames[idx_nan | (durations < cutoffs[0]) | (durations > cutoffs[1])]):
+        print(pathname, file=file)
 
 
 def main(args):
-    for path in args.path:
-        try:
-            for file in main_per_path(path):
-                print(file, file=args.output)
-        except ValueError:
-            pass
+    deque(map(partial(main_per_path, file=args.output), args.dirs), 0)
 
 
 def cli():
     parser = argparse.ArgumentParser(description="Detect extreme video duration within a directory.")
 
-    parser.add_argument('path', nargs='+',
-                        help='glob patterns of files/directories.')
+    parser.add_argument('dirs', nargs='+',
+                        help='Directories.')
     parser.add_argument('-o', '--output', type=argparse.FileType('w'),
                         default=sys.stdout)
     parser.add_argument('-v', '--version', action='version',
