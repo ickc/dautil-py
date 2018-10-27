aecbf2e7997418d5288c94ce010301ed3916ad28
Kolen <christian.kolen@gmail.com>
2018-10-26 17:14:08 -0700

    pkldelete.py: merge h5delete.py changes; check EOFError instead

--- bin/pkldelete.py
+++ bin/pkldelete.py
@@ -8,39 +8,61 @@ One reason of this happening is when the file is saved, it got interupted/termin
 from __future__ import print_function
 
 import argparse
+from itertools import chain
+from glob import iglob as glob
 import os
+from functools import partial
 import sys
 
 from dautil.IO import read_pkl2
+from dautil.util import map_parallel
 
-__version__ = '0.1'
+__version__ = '0.2'
+
+
+def pkldelete(filename, dry_run=False, verbose=False):
+    try:
+        read_pkl2(filename)
+        if verbose:
+            print(filename, 'is good.')
+    except EOFError:
+        if verbose:
+            print(filename, 'is not good, and will be deleted.', file=sys.stderr)
+        else:
+            print(filename, file=sys.stderr)
+        if not dry_run:
+            os.remove(filename)
 
 
 def main(args):
-    for filename in args.input:
-        try:
-            read_pkl2(filename)
-            if args.verbose:
-                print(filename, 'is good.')
-        except IOError:
-            if args.verbose:
-                print(filename, 'is not good, and will be deleted.', file=sys.stderr)
-            else:
-                print(filename, file=sys.stderr)
-            if not args.dry_run:
-                os.remove(filename)
+    _glob = partial(glob, recursive=True) if args.recursive else glob
+    pkl_in_paths = chain(*(_glob(glob_i)
+                           for glob_i in args.input))
+
+    Nones = map_parallel(
+        partial(pkldelete, dry_run=args.dry_run, verbose=args.verbose),
+        pkl_in_paths,
+        processes=args.p
+    )
+    if args.verbose:
+        print('Finish checking {} pkl files.'.format(len(Nones)))
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Delete pickle input if it cannot be opened.')
+    parser = argparse.ArgumentParser(description='Delete pickle input if it is invalid.')
 
     parser.add_argument('input', nargs='+',
-                        help='Input pickle files. Can be more than 1.')
+                        help='glob pattern of pickle files.')
+    parser.add_argument('-R', '--recursive', action='store_true',
+                        help='If specified, recursive globbing, Python 3 only.')
+
     parser.add_argument('-v', '--version', action='version',
                         version='%(prog)s {}'.format(__version__))
     parser.add_argument('-n', '--dry-run', action='store_true',
                         help='Dry run, do not delete.')
     parser.add_argument('-V', '--verbose', action='store_true',
                         help='Print info on the input files. If not selected, only the filename of the to-be-deleted files are print.')
+    parser.add_argument('-p', type=int, default=1,
+                        help='use p processes with multiprocessing. Hint: use total no. of threads available.')
 
     main(parser.parse_args())
