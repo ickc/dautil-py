da38cb60062e771ca20fcde34f8f51379862e595
Kolen <christian.kolen@gmail.com>
2018-11-04 18:26:26 -0800

    IO/__init__.py: add head_h5_dataset

--- dautil/IO/__init__.py
+++ dautil/IO/__init__.py
@@ -12,6 +12,16 @@ def read_pkl2(path):
         return pickle.load(f) if PY2 else pickle.load(f, encoding='latin1')
 
 
+def read_h5_dataset(path, dataset):
+    '''a functional thin wrapper to read hdf5 dataset
+    from path
+    '''
+    import h5py
+
+    with h5py.File(path, 'r') as f:
+        return f[dataset][:]
+
+
 def makedirs(path):
     '''makesdirs if not exist while avoiding race condition
     catch the case that path is file, whether initially or in a race condition
