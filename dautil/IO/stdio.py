b33ac0ac4f43067e67d613f6f9fd6a9b7a407c24
Kolen Cheung <christian.kolen@gmail.com>
2019-05-25 19:47:03 -0700

    stdio.py: redirect_stdout_stderr: improve error handling

--- dautil/IO/stdio.py
+++ dautil/IO/stdio.py
@@ -10,30 +10,29 @@ def redirect_stdout_stderr(f):
     '''
     @wraps(f)
     def f_decorated(*args, **kwargs):
-        stdout = kwargs.pop('stdout', None)
-        stderr = kwargs.pop('stderr', None)
-
-        if stdout:
-            stdout_original = sys.stdout
-            f_out = os.open(stdout, os.O_WRONLY | os.O_CREAT, 0o644)
-            os.dup2(f_out, sys.stdout.fileno())
-
-        if stderr:
-            stderr_original = sys.stderr
-            f_err = os.open(stderr, os.O_WRONLY | os.O_CREAT, 0o644)
-            os.dup2(f_err, sys.stderr.fileno())
-
         try:
+            stdout = kwargs.pop('stdout', None)
+            if stdout:
+                stdout_original = sys.stdout
+                f_out = os.open(stdout, os.O_WRONLY | os.O_CREAT, 0o644)
+                os.dup2(f_out, sys.stdout.fileno())
+
+            stderr = kwargs.pop('stderr', None)
+            if stderr:
+                stderr_original = sys.stderr
+                f_err = os.open(stderr, os.O_WRONLY | os.O_CREAT, 0o644)
+                os.dup2(f_err, sys.stderr.fileno())
+
             return f(*args, **kwargs)
         finally:
-            if stderr:
-                sys.stderr.flush()  # prevent buffer not saved
-                os.dup2(stderr_original.fileno(), f_err)
-                os.close(f_err)
-
             if stdout:
                 sys.stdout.flush()  # prevent buffer not saved
                 os.dup2(stdout_original.fileno(), f_out)
                 os.close(f_out)
+
+            if stderr:
+                sys.stderr.flush()  # prevent buffer not saved
+                os.dup2(stderr_original.fileno(), f_err)
+                os.close(f_err)
 
     return f_decorated
