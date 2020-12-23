a8983dd290e7cdea6e0a88df04a53c9ca0d46870
Kolen Cheung <christian.kolen@gmail.com>
2020-12-23 03:13:36 -0800

    sacct.py: lazy ALLFORMAT

--- dautil/IO/sacct.py
+++ dautil/IO/sacct.py
@@ -9,13 +9,21 @@ if PY2:
 else:
     from io import StringIO
 
-ALLFORMAT = subprocess.run(['sacct', '--helpformat'], stdout=subprocess.PIPE).stdout.decode('utf-8').split()
+ALLFORMAT = None
+
+
+def _init_allformat():
+    global ALLFORMAT
+    ALLFORMAT = subprocess.run(['sacct', '--helpformat'], stdout=subprocess.PIPE).stdout.decode('utf-8').split()
 
 
 def get_job(jobid):
     '''run ``sacct`` for the given jobid
     and return a DataFrame of that.
     '''
+    if ALLFORMAT is None:
+        _init_allformat()
+
     result = subprocess.run(
         [
             'sacct',
@@ -33,6 +41,9 @@ def get_user(username):
     '''run ``sacct`` for the given username
     and return a DataFrame of that.
     '''
+    if ALLFORMAT is None:
+        _init_allformat()
+
     result = subprocess.run(
         [
             'sacct',
